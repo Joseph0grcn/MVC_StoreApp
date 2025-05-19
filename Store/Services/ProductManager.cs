7d0ccfc33fbe34b38595616ebc34dfae35d4a2eb
yusuf gürcan <yusuf.gurcan.34@hotmail.com>
2025-05-19 23:29:40 +0300

    Tag Helpers Completed

--- Store/Services/ProductManager.cs
+++ Store/Services/ProductManager.cs
@@ -46,7 +46,15 @@ namespace Services
            return _manager.Product.GetAllProductsWithDetails(p);
         }
 
-        public Product? GetOneProduct(int id, bool trackChanges)
+		public IEnumerable<Product> GetLastestProducts(int n, bool trackChanges)
+		{
+			return _manager.Product
+                .FindAll(trackChanges)
+                .OrderByDescending(prd => prd.Id)
+                .Take(n);
+		}
+
+		public Product? GetOneProduct(int id, bool trackChanges)
         {
             var product = _manager.Product.GetOneProduct(id, trackChanges);
             if (product is null){
